ab2989f59412e56de03da92b0193b6b52ba9dcc7
Erlangga Nugraha <erlanggawijaya17@gmail.com>
2017-11-10 15:42:50 +0700

    Fix bug pelaksana sedang melakukan tugas

--- application/views/theme_laporan.php
+++ application/views/theme_laporan.php
@@ -71,7 +71,7 @@ desired effect
   <header class="main-header">
 
     <!-- Logo -->
-    <a href="index2.html" class="logo">
+    <a href="#" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
       <span class="logo-mini"><b>A</b>PD</span>
       <!-- logo for regular state and mobile devices -->
